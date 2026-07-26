c215e26d1cc5c5414c0435a902fab13cfa4a1896
Jagadeesh Pinniboina <pinniboinajagadeesh76@gmail.com>
2026-07-26 18:08:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const themeToggle = document.getElementById("theme-toggle");
 
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+        themeToggle.textContent = "☀️";
+    } else {
+        themeToggle.textContent = "🌙";
+    }
+});
